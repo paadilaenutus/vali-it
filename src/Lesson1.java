4b0d9755e2927886cd1b71a8ba4065953f96de9c
paadilaenutus <toomistu@gmail.com>
2020-08-24 14:48:20 +0300

    Lesson1 Lesson1MathUtil DONE

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -10,33 +10,59 @@ public class Lesson1 {
     public static void excersie1(){
         // TODO
         // defineeri 3 muutujat a = 1, b = 1, c = 3
-        // Prindi välja a==b ja a==c
+        int a = 1;
+        int b = 1;
+        int c = 3;
+        // Prindi v2lja a==b ja a==c
+        System.out.println(a==b);
+        System.out.println(a==c);
         // Lisa rida a = c
-        // Prindi välja a==b ja a==c, mis muutus???
+        a = c;
+        // Prindi v2lja a==b ja a==c, mis muutus???
+        System.out.println(a==b);
+        System.out.println(a==c);
     }
 
     public static void excersie2(){
         // TODO
-        // Loo muutujad x1 = 10 ja x2 = 20, vali sobiv andmetüüp
-        // Tekita muutuja y1 = ++x1, trüki välja nii x1 kui y1
-        // Tekita muutuja y2 = x2++, trüki välja nii x2 ja y2
-        // Analüüsi tulemusi
+        // Loo muutujad x1 = 10 ja x2 = 20, vali sobiv andmetyyp
+        int x1 = 10;
+        int x2 = 20;
+        // Tekita muutuja y1 = ++x1, tryki v2lja nii x1 kui y1
+        int y1 = ++x1;
+        System.out.println(x1);
+        System.out.println(y1);
+        // Tekita muutuja y2 = x2++, tryki v2lja nii x2 ja y2
+        int y2 = x2++;
+        System.out.println(x2);
+        System.out.println(y2);
+        // Analyysi tulemusi
     }
 
     public static void excersie3(){
         // TODO
         // Loo arvulised muutujad
         // a = 18 % 3
+        int a = 18 % 3;
         // b = 19 % 3
+        int b = 19 % 3;
         // c = 20 % 3
+        int c = 20 % 3;
         // d = 21 % 3
-        // Prindi välja kõigi muutujate väärtused
+        int d = 21 % 3;
+        // Prindi v2lja k6igi muutujate v22rtused
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
     }
 
     public static void excersie4(){
         // TODO
-        // Defineeri String tüüpi muutuja mille sisu oleks „\“\\““
-        // Trüki muutuja sisu välja
+        // Defineeri String tyypi muutuja mille sisu oleks "\"\\""
+        String s = "\"\\\"\\\\\"\"";
+        // Tryki muutuja sisu v2lja
+        System.out.println(s);
     }
 
 }
--- src/Lesson1MathUtil.java
+++ src/Lesson1MathUtil.java
@@ -1,38 +1,67 @@
+import java.sql.SQLOutput;
+
 public class Lesson1MathUtil {
 
     public static void main(String[] args) {
-
+        int minimum = min(-3, 5);
+        int maximum = max(3, 5);
+        int absolute = abs(-1154);
+        boolean odds = isEven(888);
+        int minimum3 = min(3, 3, 5);
+        int maximum3 = max(6, 6, 4);
+        System.out.println(minimum);
+        System.out.println(maximum);
+        System.out.println(absolute);
+        System.out.println(odds);
+        System.out.println(minimum3);
+        System.out.println(maximum3);
     }
 
-    public static int min(int a, int b){
+    public static int min(int a, int b) {
         // TODO tagasta a ja b väikseim väärtus
-        return 0;
+        if (a < b) {
+            return a;
+        } else return b;
     }
 
-    public static int max(int a, int b){
+    public static int max(int a, int b) {
         // TODO tagasta a ja b suurim väärtus
-        return 0;
+        if (a > b) {
+            return a;
+        } else return b;
     }
 
-    public static int abs(int a){
+    public static int abs(int a) {
         // TODO tagasta a absoluut arv
-        return 0;
+        if (a < 0) {
+            a *= -1;
+        } return a;
     }
 
-    public static boolean isEven(int a){
+    public static boolean isEven(int a) {
         // TODO tagasta true, kui a on paaris arv
         // tagasta false kui a on paaritu arv
-        return false;
+        return (a % 2 == 0);
     }
 
-    public static int min(int a, int b, int c){
+    public static int min(int a, int b, int c) {
         // TODO tagasta a, b ja c väikseim väärtus
-        return 0;
+        if (a <= b && a <= c) {
+            return a;
+        }
+        if (b <= a && b <= c) {
+            return b;
+        } else return c;
     }
 
-    public static int max(int a, int b, int c){
+    public static int max(int a, int b, int c) {
         // TODO tagasta a, b ja c suurim väärtus
-        return 0;
+        if (a >= b && a >= c) {
+            return a;
+        }
+        if (b >= a && b >= c) {
+            return b;
+        } else return c;
     }
-
 }
+
